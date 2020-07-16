c5bc94dff5e31d19c2ae8d9880dd5488ee125940
sailfish009 <sailfish009@gmail.com>
2020-07-16 13:48:30 +0900

    fix AT_CHECK

--- mmdet/ops/roi_align/src/roi_align_cuda.cpp
+++ mmdet/ops/roi_align/src/roi_align_cuda.cpp
@@ -34,9 +34,9 @@ at::Tensor ROIAlignBackwardV2Laucher(
     const int sampling_ratio, bool aligned);
 #endif
 
-#define CHECK_CUDA(x) AT_CHECK(x.type().is_cuda(), #x, " must be a CUDAtensor ")
+#define CHECK_CUDA(x) TORCH_CHECK(x.type().is_cuda(), #x, " must be a CUDAtensor ")
 #define CHECK_CONTIGUOUS(x) \
-  AT_CHECK(x.is_contiguous(), #x, " must be contiguous ")
+  TORCH_CHECK(x.is_contiguous(), #x, " must be contiguous ")
 #define CHECK_INPUT(x) \
   CHECK_CUDA(x);       \
   CHECK_CONTIGUOUS(x)
